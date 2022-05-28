ed93af9f4deeae2d84441accd693bd2f4987b383
Hong Zhou <67214990+hongZhou443@users.noreply.github.com>
2022-05-28 16:37:31 -0700

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,8 @@
 function search_for_keyword(s_arr, keyword){
     var res = {keyword: []};
-    for (let i = 0; i < s_arr.length; i++){
+    for (var i = 0; i < s_arr.length; i++){
         if(s_arr[i].toLowerCase().includes(keyword.toLowerCase)){
-            s = s_arr[i];
+            var s = s_arr[i];
             if(i > 0){
                 s = s_arr[i-1] + s;
             }
@@ -19,19 +19,20 @@ function search_for_keyword(s_arr, keyword){
 }
 
 function parse_string(tot_string, keywords){
-    let s_arr = tot_string.split('.');
-    let f = {};
+    var s_arr = tot_string.split('.');
+    var f = {};
 
     for(var keyword in keywords){
-        key_search = parse_string(s_arr, keyword);
+        var key_search = parse_string(s_arr, keyword);
         f.push(key_search);
     }
 
-    let res = "";
+    var res = "";
+  
     for(var key in f){
         res += key + ': \n\n';
 
-        keysearch = f.get(key);
+        var keysearch = f.get(key);
         for(var section in keysearch){
             res += section + '\n';
         }
